46f869b5273bfa96567a582b7cd42b2fb790866d
Bipulz <bhandaribipul345@gmail.com>
2025-11-13 10:55:10 +0545

    Task 5 completed

---
+++ Task5/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from task5");
+
+            Console.Write("Enter any positive number: ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            int total = 0;
+
+            for (int i = 1; i <= limit; i++)
+            {
+                total += i;
+            }
+            Console.WriteLine($"Total sum from 1 to {limit} is: {total}");
+
+            int count = 1;
+            while (count <= 20)
+            {
+                // Skip multiples of 5
+                if (count % 5 == 0)
+                {
+                    count++;
+                    continue;
+                }
+
+                // Stop when count reaches 18
+                if (count == 18)
+                {
+                    break;
+                }
+
+                Console.WriteLine(count);
+                count++;
+            }
+        }
+    }
+}
