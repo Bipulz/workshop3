fc3b3ff1fdcaa2b1844fee7489a654c825c31007
Bipulz <bhandaribipul345@gmail.com>
2025-11-13 10:24:44 +0545

    task-2 completed

---
+++ Task2/NullOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    internal class NullOperations
+    {
+        public void PerformNullChecks()
+        {
+            string? cityName = null;
+
+            string resultTernary = (cityName == null) ? "City name is not provided" : cityName;
+            Console.WriteLine("Using Ternary Operator: " + resultTernary);
+
+            string resultCoalescing = cityName ?? "City name is not provided";
+            Console.WriteLine("Using Null Coalescing Operator: " + resultCoalescing);
+
+            cityName ??= "Kathmandu";
+            Console.WriteLine("Using Null Coalescing Assignment: " + cityName);
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Task2
+﻿using task2;
+
+namespace Task2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello from task2");
+
+            var nullOps = new NullOperations();
+            nullOps.PerformNullChecks();
         }
     }
 }
