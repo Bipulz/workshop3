7f92c27b59a44b80812b1230c85ef95747616600
Bipulz <bhandaribipul345@gmail.com>
2025-11-13 10:18:35 +0545

    task 1 complete

---
+++ Task1/Operators.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Operators
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+
+        }
+
+        public int Multiply(int a, int b)
+        {
+            
+            return a * b;
+        }
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cant be zero!");
+            }
+            return (double)a / b;
+        }
+
+        public void EvenOddFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "this is Even Number" : "this is Odd Number";
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var ops = new Operators();
+
+            int a = 25;
+            int b = 18;
+            int num = 88;
+
+            Console.WriteLine($"Sum is: {ops.Add(a , b)}");
+            Console.WriteLine($"Difference is: {ops.Subtract(a, b)}");
+            Console.WriteLine($"Product is: {ops.Multiply(a , b)}");
+            Console.WriteLine($"Divide: {ops.Divide(a, b)}");
+
+            ops.EvenOddFinder(num);
+        }
+    }
+}
